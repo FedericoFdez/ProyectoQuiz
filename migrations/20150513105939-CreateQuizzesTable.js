900dd560e26f7b068c102cf2930d932efe36e748
Federico Fdez <fa.fernandez@alumnos.upm.es>
2015-05-13 19:39:05 +0200

    Migraciones añadidas

---
+++ migrations/20150513105939-CreateQuizzesTable.js
@@ -0,0 +1,23 @@
+module.exports = {
+  up: function (migration, DataTypes, done) {
+        migration.createTable(
+          'Quiz', { id:       { type: DataTypes.INTEGER,
+                                allowNull: false,
+                                primaryKey: true,
+                                autoIncrement: true,
+                                unique: true },
+                    pregunta: { type: DataTypes.STRING},
+                    respuesta:{ type: DataTypes.STRING},
+                    createdAt:{ type: DataTypes.DATE,
+                                allowNull: false},
+                    updatedAt:{ type: DataTypes.DATE,
+                                allowNull: false}
+                  },
+                  { sync: {force:true} })
+        .complete(done);
+  },
+    down: function (migration, DataTypes, done) {
+      migration.dropTable('Posts')
+      .complete(done);
+    }
+  }
--- models/models.js
+++ models/models.js
@@ -32,18 +32,17 @@ var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz 
 
-// sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().then(function() {
-	//then(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().then(function (count){
-		if(count === 0) {	// la tabla se inicializa solo si está vacía
-			Quiz.create({pregunta: 'Capital de Italia', 
-						 respuesta: 'Roma'
-						});
-			Quiz.create({pregunta: 'Capital de Portugal',
-						 respuesta: 'Lisboa'
-						})
-			.then(function(){console.log('Base de datos inicializada')});
-		};
-	});
+Quiz.count().then(function (count){
+	if(count === 0) {	// la tabla se inicializa solo si está vacía
+		Quiz.create({pregunta: 'Capital de Italia', 
+					 respuesta: 'Roma'
+					});
+		Quiz.create({pregunta: 'Capital de Portugal',
+					 respuesta: 'Lisboa'
+					});
+		Quiz.create({pregunta: 'Capital de Francia',
+					 respuesta: 'París'
+					})
+		.then(function(){console.log('Base de datos inicializada')});
+	};
 });
