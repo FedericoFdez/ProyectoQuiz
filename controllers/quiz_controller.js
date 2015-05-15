1ae956ac5cd5ad5bb4c6583d098bdc506342083f
Federico Fdez <fa.fernandez@alumnos.upm.es>
2015-05-15 14:07:25 +0200

    Validación de preguntas añadida
    Ahora se realiza una validación de los campos introducidos para que no
    estén en blanco. Modificaciones menores en modelo de base de datos y en
    migraciones para que todo funcione correctamente.
    Estilos añadidos para mostrar los errores.
    Modificaciones menores para nuevo layout con errores.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -18,12 +18,12 @@ exports.index = function(req, res) {
 		models.Quiz.findAll({	where: ["pregunta like ?", req.query.search.replace(/(\s)/g,'%').replace(/^/,'%').replace(/$/,'%')],
 								order: 'pregunta ASC'
 							}).then(function(quizes) {
-			res.render('quizes/index.ejs',{quizes:quizes, busqueda:req.query.search});
+			res.render('quizes/index.ejs',{quizes:quizes, busqueda:req.query.search, errors:[]});
 		});
 	}
 	else{
 		models.Quiz.findAll().then(function(quizes) {
-			res.render('quizes/index.ejs', {quizes: quizes, busqueda: req.query.search});
+			res.render('quizes/index.ejs', {quizes: quizes, busqueda: req.query.search, errors:[]});
 		});
 	}
 };
@@ -33,23 +33,31 @@ exports.new = function(req,res) {
 	var quiz = models.Quiz.build( //crea objeto quiz
 		{pregunta: "Pregunta", respuesta: "Respuesta"}
 		);
-	res.render('quizes/new',{quiz:quiz});
+	res.render('quizes/new',{quiz:quiz, errors:[]});
 };
 
 // POST /quizes/create
 exports.create = function(req,res) {
-	var quiz = models.Quiz.build(req.body.quiz);
-
-	// guarda en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	}) // Redirección HTTP (URL relativo) a la lista de preguntas
+	var quiz = models.Quiz.build( req.body.quiz );
+	quiz.validate()
+	.then(
+		function(err){
+			if(err) {
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			} else {
+				quiz // guarda en DB los campos pregunta y respuesta de quiz
+				.save({fields: ["pregunta", "respuesta"]})
+				.then(function(){ res.redirect('/quizes')});
+					// Redirección HTTP (URL relativo) a la lista de preguntas
+			}
+		}
+	);
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
 	models.Quiz.find(req.params.quizId).then(function(quiz) {
-		res.render('quizes/show', { quiz: req.quiz});	
+		res.render('quizes/show', { quiz: req.quiz, errors:[]});	
 	})
 };
 
@@ -57,9 +65,9 @@ exports.show = function(req, res) {
 exports.answer = function(req, res) {
 	models.Quiz.find(req.params.quizId).then(function(quiz) {
 		if (req.query.respuesta === req.quiz.respuesta) {
-			res.render('quizes/answer', { quiz:req.quiz, respuesta: 'Correcto' });
+			res.render('quizes/answer', { quiz:req.quiz, respuesta: 'Correcto', errors:[] });
 		} else {
-			res.render('quizes/answer', { quiz: req.quiz, respuesta: 'Incorrecto' });
+			res.render('quizes/answer', { quiz: req.quiz, respuesta: 'Incorrecto', errors:[] });
 		}
 	})
 };
--- migrations/20150513105939-CreateQuizzesTable.js
+++ migrations/20150513105939-CreateQuizzesTable.js
@@ -1,23 +1,27 @@
 module.exports = {
   up: function (migration, DataTypes, done) {
         migration.createTable(
-          'Quizzes', { id:       { type: DataTypes.INTEGER,
+          'Quizzes', { id:    { type: DataTypes.INTEGER,
                                 allowNull: false,
                                 primaryKey: true,
                                 autoIncrement: true,
                                 unique: true },
-                    pregunta: { type: DataTypes.STRING},
-                    respuesta:{ type: DataTypes.STRING},
+                    pregunta: { type: DataTypes.STRING,
+                                validate: { notEmpty: {msg: "· Falta Pregunta"}}
+                              },
+                    respuesta:{ type: DataTypes.STRING,
+                                validate: { notEmpty: {msg: "· Falta Respuesta"}}
+                              },
                     createdAt:{ type: DataTypes.DATE,
                                 allowNull: false},
                     updatedAt:{ type: DataTypes.DATE,
                                 allowNull: false}
                   },
-                  { sync: {force:true} })
-        .complete(done);
+                  { sync: {force:true} });
+        done();
   },
     down: function (migration, DataTypes, done) {
-      migration.dropTable('Quizzes')
-      .complete(done);
+      migration.dropTable('Quizzes');
+      done();
     }
   }
